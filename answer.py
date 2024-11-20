a687a1775c1e77f2a2d04cfeccfcdbb8eac38b84
wang2135 <wang2135@e.ntu.edu.sg>
2024-11-20 06:24:41 +0000

    1

--- answer.py
+++ answer.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
-from flask import render_template, request
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask("__name__")
 
+# 根路由，自动跳转到 /qa
+@app.route("/", methods=["GET"])
+def home():
+    return redirect(url_for("qa"))
+
+# /qa 路由，渲染 Q&A.html
 @app.route("/qa", methods=["GET"])
 def qa():
     return render_template("myanswer.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
